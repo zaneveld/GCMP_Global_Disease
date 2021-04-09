152cdc70fc85314a88f3f8654c247b645793a0cf
Hannah Epstein <hannah@Hannahs-MacBook-Pro.local>
2021-04-09 12:29:18 -0700

    update to gdm

--- analysis/decontamination/procedure/biom_tre-to-qza.sh
+++ analysis/decontamination/procedure/biom_tre-to-qza.sh
@@ -8,3 +8,9 @@ qiime tools import \
   --input-path physeq.noncont_tree-rooted.newick \
   --output-path physeq.noncton-rooted-tree.qza \
   --type 'Phylogeny[Rooted]'
+  
+qiime tools import \
+    --input-path ~/Documents/OSUDocs/Projects/Disease_LHS/GCMP_Global_Disease/analysis/decontamination/output/physeq.noncont_tax.txt \
+    --output-path ~/Documents/OSUDocs/Projects/Disease_LHS/GCMP_Global_Disease/analysis/decontamination/output/physeq.noncont-tax.qza \
+    --input-format HeaderlessTSVTaxonomyFormat \
+    --type 'FeatureData[Taxonomy]'
