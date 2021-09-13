50b2ce4ab55e98cbdec3f6bb64049e40f31c130e
zaneveld <zaneveld@gmail.com>
2021-09-13 13:27:44 -0700

    Added additional documentation and removed extra print statements

--- analysis/core_analysis/procedure/asr_R_trial.R
+++ analysis/core_analysis/procedure/asr_R_trial.R
@@ -1,3 +1,7 @@
+#Goal: This script identifies the most recent common ancestor (MRCA) of each
+#genus in the analysis to help with data merging at the genus level.
+
+#Uncomment to install packages
 #install.packages("tidyr",repos='http://cran.us.r-project.org')
 #install.packages("phytools",repos='http://cran.us.r-project.org')
 #install.packages("dplyr",repos='http://cran.us.r-project.org')
@@ -10,20 +14,18 @@ map <- read.table("../input/GCMP_EMP_map_r29.txt",sep="\t",header=TRUE,fill=TRUE
 coral_tree <- read.tree("../input/huang_roy_molecular.newick") 
 
 
-
+#Filter out samples with no binomial name
 map <- map %>% filter(sample_type_EMP != "control blank" )
 map <- map %>% filter(sample_type_EMP != "" )
 map <- map %>% filter(host_species_id != "Missing: Not collected" )
 map <- map %>% filter(host_species_id != "Not applicable" )
 
-print(map)
+#Split scientific name into genus and species, filling missing with NA
 map <- map %>% separate("host_scientific_name", into = c("Genus", "Species"), sep = " ", remove = FALSE, extra = "merge")
-#View(map$Genus)
 
-mrca_full <- mrca(coral_tree, full = FALSE)
-write.csv(mrca_full, "../output/huang_roy_full_mrca_table.csv")
-
+#Collect the unique genus names to iterate over
 unique_genera <- unique(map$Genus)
+
 #make a vector mapping genus to node number
 genus_mrcas = c()
 genus_names = c()
@@ -32,11 +34,19 @@ for (unique_genus in unique_genera) {
   print(unique_genus)
   genus_map <- map %>% filter(Genus == unique_genus)
   species_in_genus <- unique(genus_map$Huang_Roy_tree_name)
+  
+  #skip genera that are in the GCMP mapping file but 
+  #not in the Huang Roy tree
   if("none" %in% species_in_genus) {
     print("Skipping!")
     next
   }
-  print(length(species_in_genus))
+
+  #We need special handling of genera with one species
+  #in the dataset. These cause an error in findMRCA.
+  #To avoid that we set the species as representative of the 
+  #genus in these cases. 
+  print(paste("Species in this genus:",length(species_in_genus)))
   if (length(species_in_genus) == 1){
     print("only 1 species in this genus!")
     print(paste("Species in genus:",species_in_genus))
@@ -48,6 +58,8 @@ for (unique_genus in unique_genera) {
     genus_MRCA <- findMRCA(coral_tree, tips = species_in_genus,type="node")
     print(paste("Genus MRCA:", genus_MRCA))
   }
+  
+  #Add the results to our vectors of genera and their names
   genus_mrcas <- c(genus_mrcas,genus_MRCA)
   genus_names <- c(genus_names,unique_genus)
 }
@@ -59,5 +71,6 @@ for (genus_name in genus_names) {
     i <- i + 1
 }
 
+#Write results to file
 df <- data.frame(genus_mrcas,row.names=genus_names)
 write.csv(df,'../output/genus_to_mrca_mapping.csv')
