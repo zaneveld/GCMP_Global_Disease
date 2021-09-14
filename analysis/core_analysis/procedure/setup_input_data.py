c8cc7f0ed36fa3c19eba52b10d3cdcdbb76cb7ae
zaneveld <zaneveld@gmail.com>
2021-09-14 11:55:23 -0700

    Renamed asr_R_trial find_MRCA_by_genus.R

--- analysis/core_analysis/procedure/setup_input_data.py
+++
@@ -1,7 +0,0 @@
-#Copy input data into the input folder
-
-cp ../../decontamination/output/physeq.noncton-rooted-tree.qza ../input/physeq_rooted_tree.qza
-cp ../../decontamination/output/physeq.noncont-feature-table.qza ../input/physeq_feature_table.qza
-cp ../../decontamination/output/physeq.noncont_sample-metadata.txt ../input/physeq_metadata.txt
-cp ../../decontamination/output/physeq.noncont_tax.txt ../input/physeq_taxonomy.txt
-
