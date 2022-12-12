b17fcab002785857f57eace77b1c6ffc9d9e8c82
Hannah Epstein <hannah@hannahs-mbp.lan>
2022-12-12 10:23:00 -0800

    adding proposed figure drafts and path analysis output

--- analysis/core_analysis/procedure/path_analysis.R
+++ analysis/core_analysis/procedure/path_analysis.R
@@ -20,18 +20,15 @@ filtered_table <- subset(coral_disease_data, select = c("growth_rate_mm_per_year
 colnames(filtered_table) <- c("GR","EN","DS")
 filtered_table
 
-#Define the models:
+#Define the possible (biologically logical) models:
 # __ notates causation ( B <- A, aka B is caused by A )
 models <- define_model_set(
   EN__DS_plus_GR    = c(EN ~ DS + GR),
-  #two    = c(EN ~ DS + GR, GR ~ DS),
   EN__GR__DS  = c(EN ~ GR, GR ~ DS),
   GR__DS__EN   = c(GR ~ DS, DS ~ EN),
   GR__DS_plus_EN  = c(GR ~ DS + EN),
-  #six   = c(GR ~ DS + EN, EN ~ DS),
   DS__EN__GR  = c(DS ~ EN, EN ~ GR),
   DS__GR__EN  = c(DS ~ GR, GR ~ EN),
-  #nine   = c(DS ~ GR + EN, EN ~ GR)
   EN__GR    = c(EN ~ GR),
   GR__EN = c(GR ~ EN),
   GR__DS = c(GR ~ DS),
@@ -42,43 +39,54 @@ models <- define_model_set(
   DS_and_GR__EN = c(DS ~ EN, GR ~ EN)
 )
 
+#Plot models & save
 plot_model_set(models)
 ggsave("../output/phylopath/model_set.pdf", plot = last_plot())
 
-#Evaluation of hypotheses
+#Evaluation of hypotheses using both Brownian (BM) and Pagel's Lamba (PL) evolutionary models
 p_BM <- phylo_path(models, data = filtered_table, tree = coral_tree, model = "BM")
 p_PL <- phylo_path(models, data = filtered_table, tree = coral_tree, model = "lambda")
 
+#Summary results & graph of support for each model 
+#Brownian
 s_BM <- summary(p_BM)
 s_BM
-s_PL <- summary(p_PL)
-s_PL
-
-#Summary of support for each model
-
+write.csv(s_BM, "../output/phylopath/phylopath_summary_BM.csv")
 plot(s_BM)
 ggsave("../output/phylopath/phylopath_model_summary_BM.pdf", plot = last_plot())
+
+#Pagel's Lambda
+s_PL <- summary(p_PL)
+s_PL
+write.csv(s_PL, "../output/phylopath/phylopath_summary_PL.csv")
 plot(s_PL)
 ggsave("../output/phylopath/phylopath_model_summary_PL.pdf", plot = last_plot())
 
+
 #Selecting and fitting a final model
+#First look at the "best" model fit (although be aware of CICc criteria)
 #Brownian Motion
 best_model_BM <- best(p_BM)
 best_model_BM
 plot(best_model_BM)
 ggsave("../output/phylopath/phylopath_best_model_BM.pdf", plot = last_plot())
 
-#Pagal's lambda
+#Pagel's Lambda
 best_model_PL <- best(p_PL)
 best_model_PL
 plot(best_model_PL)
 ggsave("../output/phylopath/phylopath_best_model_PL.pdf", plot = last_plot())
 
+#Next check the "average" models, which likely better represent the causality
+#due to the multiple model fits as determined by CICc
 
 #Average models:
+#Brownian
 average_model_BM <- average(p_BM, avg_method = "full")
 plot(average_model_BM, algorithm = "mds", curvature = 0.1)
 ggsave("../output/phylopath/phylopath_average_model_BM.pdf", plot = last_plot())
+
+#Pagel's Lambda
 average_model_PL <- average(p_PL, avg_method = "full")
 plot(average_model_PL, algorithm = "mds", curvature = 0.1)
 ggsave("../output/phylopath/phylopath_average_model_PL.pdf", plot = last_plot())
