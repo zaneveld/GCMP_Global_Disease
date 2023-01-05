04c422c8bbe3673f704ee5f219dc031d3a317962
zaneveld <zaneveld@gmail.com>
2023-01-04 19:28:56 -0800

    Updated graphics for PICs, added null expectation line, results now save to PIC_results folder in output

--- analysis/core_analysis/procedure/phylomorphospace_r14.r
+++ analysis/core_analysis/procedure/phylomorphospace_r14.r
@@ -1,4 +1,8 @@
-#Output results to a log file as well as to the screen
+
+#A script for running phytools PIC and phylomorphospace analysis from
+#the command line
+
+#See below for usage. 
 print("Usage: Rscript phylomorphospace_r14.r <path_to_trait_table> <path_to_tree> <x_trait_column> <y_trait_column>")
 
 library(ggplot2)
@@ -10,7 +14,7 @@ tree_fp <- args[2]
 x_trait <-args[3]
 y_trait <-args[4]
 
-
+#Filtering is not accessible from the commandline, but can be set in the script
 #Example of how to filter results to just genera
 #with Gammaproteobacteria as the dominant class
 
@@ -19,8 +23,13 @@ y_trait <-args[4]
 filter_column <- NA
 filter_value <- NA
 
+filter_column <- args[5]
+filter_value <- args[6]
 
-output_dir <- paste0("../output/phyl_corr_tests_",x_trait,"_vs_",y_trait)
+output_dir_for_all_pics = "../output/PIC_results/"
+dir.create(output_dir_for_all_pics, showWarnings = FALSE)
+
+output_dir <- paste0(output_dir_for_all_pics,"PIC_",x_trait,"_vs_",y_trait)
 if (!is.na(filter_column)){
     output_dir <- paste0(output_dir,"_",filter_column,"_is_",gsub("\\;","_",filter_value))
 }
@@ -29,9 +38,17 @@ output_dir <- paste0(output_dir,"/")
 print(paste("Outputting results to:",output_dir))
 dir.create(output_dir)
 
+#Output results to a log file as well as to the screen
 sink(paste0(output_dir,x_trait,"_vs_",y_trait,"_results_log.txt"),append=FALSE,split=TRUE)
 
+print("Phylogenetic Independent Contrast Analysis Report")
+print("--------------------------------------------------")
 print(paste("Analyzing",x_trait,"vs.",y_trait))
+print(paste("Trait table filepath:",trait_table_fp))
+print(paste("Tree filepath:",tree_fp))
+print(paste("Filtering data based on column:",filter_column))
+print(paste("Including data only if filter column value is:",filter_value))
+print("--------------------------------------------------")
 
 trait_table <- read.table(trait_table_fp,comment.char="",header=T,row.names=1,as.is=T,sep="\t")
 tree <- read.tree(tree_fp)
@@ -77,8 +94,6 @@ write.table(trait_table,paste0(output_dir,x_trait,"_vs_",y_trait,"_filtered_tabl
 print("Calculating PICs")
 raw.pic.X <- pic(X,tree)
 raw.pic.Y <- pic(Y,tree)
-rank.pic.X <- pic(rank(X),tree)
-rank.pic.Y <- pic(rank(Y),tree)
 
 #****POSITIVIZE PIC VALUES (negative signs are arbitrary)
 #Note: this is not just the absolute value since the difference x=-1 y=2
@@ -99,24 +114,13 @@ positivized.results <- PositivizeContrasts(raw.pic.X, raw.pic.Y)
 pic.X <-positivized.results[,1]
 pic.Y <-positivized.results[,2]
 
-positivized.results <- PositivizeContrasts(rank.pic.X, rank.pic.Y)
-rank.pic.X <-positivized.results[,1]
-rank.pic.Y <-positivized.results[,2]
 pic_df <- data.frame(pic.X,pic.Y)
-pic_df$rank.pic.X <- rank.pic.X
-pic_df$rank.pic.Y <- rank.pic.Y
-
 
 # regress through origin (e.g. expect that 0 change in one trait is on average correlated with zero change in the other)
 
 fitYX <- lm(pic.Y ~ pic.X -1 )
 print(paste("Summary lm pic.Y ~ pic.X -1 for" ,x_trait,"(x) vs. ",y_trait,"(y)"))
 print(summary(fitYX))
-
-#do a spearman regression
-rank.fitYX <- lm(pic_df$rank.pic.Y ~ pic_df$rank.pic.X -1 )
-print(paste("Summary Spearman lm rank(pic.Y) ~ rank(pic.X) -1 PICs" ,x_trait,"(x) vs. ",y_trait,"(y)"))
-print(summary(rank.fitYX))
 
 ## this is a projection of the tree into morphospace
 ##This code snippit is adapted from a phytools tutorial (http://www.phytools.org/Cordoba2017/ex/3/PICs.html)
@@ -138,21 +142,45 @@ pdf(paste0(output_dir,x_trait,"_vs_",y_trait,"_pic_scatter_YX.pdf"))
 print("Dataframe for PIC analysis:")
 print(pic_df)
 
+#Do some quick calculations to figure out the y-axis limits
+#We want them symmetrical above and below the null 0 slope expectation
+
+largest_limit = max(abs(min(pic.Y)),abs(max(pic.Y)))
+
+#largest limit must be positive, so set the lower bound
+#to be that far below 0
+lower_bound = largest_limit * -1
+upper_bound = largest_limit 
+
+#Set the null model label 75% of the way along the axis
+null_model_label_x_pos = max(pic.X)*0.75 
+null_model_label_y_pos = 0 + 0.05*upper_bound
+
+fontsize = 16
+
 ggplot(pic_df, aes(pic.X,pic.Y)) + 
-    geom_smooth(method = "lm", se = TRUE, col = "black",formula = y ~ x -1) +
-    geom_point(size = 3, col = "firebrick") + 
-    labs(x = paste("Contrast in ",x_trait), y = paste("Contrast in ",y_trait)) + 
-    theme_classic()
+    geom_smooth(method = "lm",se=TRUE,level=0.95,col = "black",fill="cyan",formula = y ~ x -1) +
+    geom_point(size = 3, shape=21, fill = "lightgrey",colour = "black",alpha=0.5) + 
+    labs(x = paste("Contrast in ",x_trait), y = paste("Contrast in ",y_trait))+
+    scale_y_continuous(limits=c(lower_bound,upper_bound))+ 
+    geom_hline(yintercept=0, color = "firebrick", size=1, linetype="dotted")+
+    geom_label(
+        label="Null expectation", 
+        x=null_model_label_x_pos,
+        y=null_model_label_y_pos,
+        label.size = NA,
+        size = 6, #not a font size, but a point size
+        color = "firebrick",
+        face = "bold",
+        fill = NA
+    )+
+    
+    theme_classic()+
+    theme(axis.text=element_text(size=16))+
+    theme(axis.title=element_text(size=16))+
+    theme(text = element_text(family = "sans"))
+
 dev.off()
-
-#Save rank PIC contrasts as a pdf
-
-pdf(paste0(output_dir,x_trait,"_vs_",y_trait,"_pic_rank_scatter_YX.pdf"))
-ggplot(pic_df, aes(rank.pic.X,rank.pic.Y)) + 
-    geom_smooth(method = "lm", se = TRUE, col = "black",formula = y ~ x -1) +
-    geom_point(size = 3, col = "firebrick") + 
-    labs(x = paste("Contrast in ",x_trait), y = paste("Contrast in ",y_trait)) + 
-    theme_classic()
 
 #Build contmap for trait X
 fit<-fastAnc(tree,X,vars=TRUE,CI=TRUE)
